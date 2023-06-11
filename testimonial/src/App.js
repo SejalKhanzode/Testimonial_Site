2a9981a1b9798b4e88a5c6374c6edc2f5a9891a7
Sejal Khanzode <khanzodesejal@gmail.com>
2023-06-11 16:12:16 +0530

    firstcomment

---
+++ testimonial/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Testimionial from "./components/Testimonials"
+
+const App = () => {
+  return(
+    <div>
+      <div>
+        <h1>Our Testimonial</h1>
+        <div></div>
+        <Testimionial/>
+      </div>
+    </div>
+  ) ;
+};
+
+export default App;
